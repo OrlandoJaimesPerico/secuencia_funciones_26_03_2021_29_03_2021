6fdc680addfbf7ae211c9871e232b4334dcaa914
OrlandoJaimesPerico <orjapejea@gmail.com>
2021-03-29 19:18:42 -0500

    29/03/2021

---
+++ main.js
@@ -0,0 +1,55 @@
+
+
+
+//Codigo en función
+function Saludar(p1){
+    console.log(`Hola ${p1}`);
+    return null;
+}
+
+Saludar("Miguel Angel");
+Saludar("Marly");
+Saludar("Orlando");
+Saludar("Fabian");
+
+
+// Codigo en secuencia
+let saludar;
+saludar = "Miguel Angel";
+console.warn    (`Hola ${saludar}`);
+saludar = "Marly";
+console.warn    (`Hola ${saludar}`);
+saludar = "Orlando";
+console.warn    (`Hola ${saludar}`);
+saludar = "Fabian";
+console.warn    (`Hola ${saludar}`);
+
+
+
+
+
+
+
+
+
+
+
+
+//debugger;
+//let nombre;
+//let edad;
+//nombre = prompt("Ingrese su nombre");
+//edad =parseInt(prompt("Ingrese su edad"));
+//console.log(nombre);
+//console.log(typeof(nombre));
+//console.log(edad);
+//console.log(typeof(edad));
+
+
+
+
+
+
+
+
+
